97f228b9dec92fe3e10253d7c9d9e41228ad0288
David De Sloovere <david.desloovere@outlook.com>
2023-10-20 20:51:09 +0200

    remove nitro preset
    not required I think

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ export default defineNuxtConfig({
   modules: [
     "@nuxt/image"
   ],
-  nitro: {
-    preset: "cloudflare-pages"
-  },
   experimental: {
     // https://github.com/unjs/nitro/issues/1844
     appManifest: false
