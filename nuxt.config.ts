26a722e82028f18f57cf2f8822eb1146df380a15
David De Sloovere <david.desloovere@outlook.com>
2023-10-20 21:04:01 +0200

    Update nuxt.config.ts - empty CF config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@ export default defineNuxtConfig({
   modules: [
     "@nuxt/image"
   ],
+  image: {
+    cloudflare: {
+      baseURL: ''
+    }
+  },
   experimental: {
     // https://github.com/unjs/nitro/issues/1844
     appManifest: false
