1a73cd3ad019b2c815a2d15f9d39ae28336a3686
David De Sloovere <david.desloovere@outlook.com>
2023-10-20 21:06:51 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,9 +3,7 @@
     <h1>Nuxt Image Test</h1>
     <NuxtImg src="/cat.jpg" width="1024" quality="1" format="webp" />
 
-    <hr>
-    <NuxtImg provider="cloudflare" src="/cat.jpg" width="1024" quality="1" format="webp" />
-
+   
     <hr>
     Orig<br>
         <img src="/cat.jpg" width="1024"  />
