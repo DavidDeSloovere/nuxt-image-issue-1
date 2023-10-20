b6fc8e2a3eb9bba8651da070e9f53d5d526c047f
David De Sloovere <david.desloovere@outlook.com>
2023-10-20 20:57:02 +0200

    Add original image

--- app.vue
+++ app.vue
@@ -2,5 +2,9 @@
   <div>
     <h1>Nuxt Image Test</h1>
     <NuxtImg src="/cat.jpg" width="1024" quality="1" format="webp" />
+
+    <hr>
+    Orig<br>
+        <img src="/cat.jpg" width="1024"  />
   </div>
 </template>
