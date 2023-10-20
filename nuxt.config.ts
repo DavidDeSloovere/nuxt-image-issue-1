7f1cc9beb82c1a14a5c7467c655850673709d1d1
David De Sloovere <david.desloovere@outlook.com>
2023-10-20 21:06:40 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,6 @@ export default defineNuxtConfig({
   modules: [
     "@nuxt/image"
   ],
-  image: {
-    cloudflare: {
-      baseURL: 'https://nuxt-image-issue-1-mry.pages.dev/'
-    }
-  },
   experimental: {
     // https://github.com/unjs/nitro/issues/1844
     appManifest: false
