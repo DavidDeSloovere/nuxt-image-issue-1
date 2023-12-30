63e53ecd43b8d9fa8ec55f0d89ead57f29471d5a
David De Sloovere <352626+DavidDeSloovere@users.noreply.github.com>
2023-12-30 14:43:30 +0100

    remove cloudflare

--- app.vue
+++ app.vue
@@ -2,9 +2,7 @@
   <div>
     <h1>Nuxt Image Test</h1>
     <NuxtImg src="/cat.jpg" width="1024" quality="1" format="webp" />
-<NuxtImg provider="cloudflare" src="/cat.jpg" width="1024" quality="1" format="webp" />
-
-   
+ 
     <hr>
     Orig<br>
         <img src="/cat.jpg" width="1024"  />
