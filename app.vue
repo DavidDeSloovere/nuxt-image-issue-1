dcc091c6f8bb4f8d1b36f0c9e0304cff08fb7a44
David De Sloovere <352626+DavidDeSloovere@users.noreply.github.com>
2023-12-30 14:41:08 +0100

    remove ipx

--- app.vue
+++ app.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>Nuxt Image Test</h1>
     <NuxtImg src="/cat.jpg" width="1024" quality="1" format="webp" />
-<NuxtImg provider="ipx" src="/cat.jpg" width="1024" quality="1" format="webp" />
 <NuxtImg provider="cloudflare" src="/cat.jpg" width="1024" quality="1" format="webp" />
 
    
